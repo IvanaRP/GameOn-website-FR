3a478cdc0f9689327f23da974ba038c095f728e0
IvanaRP <83823351+IvanaRP@users.noreply.github.com>
2021-10-16 21:47:21 +0200

    modal
    messages d'erreur

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -73,6 +73,17 @@ function editNav() {
 // (6) La case des conditions générales est cochée, l'autre case est facultative / peut être laissée décochée.
 // Conserver les données du formulaire (ne pas effacer le formulaire) lorsqu'il ne passe pas la validation.
 
+
+
+//  #3 Ajouter validation ou messages d'erreur
+
+// Des messages d'erreur spécifiques doivent apparaître sous l'entrée qui n'est pas correcte. Les messages d'erreur doivent s'afficher sous le champ de saisie associé. Exemples :
+
+// "Veuillez entrer 2 caractères ou plus pour le champ du nom."
+// "Vous devez choisir une option."
+// "Vous devez vérifier que vous acceptez les termes et conditions."
+// "Vous devez entrer votre date de naissance."
+
 document.getElementById("submitForm").addEventListener ("submit" , function (e) {
 
 
