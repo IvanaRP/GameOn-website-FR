96cdd58fe6bbe590be517c7956f737fb2c803d43
IvanaRP <83823351+IvanaRP@users.noreply.github.com>
2021-10-22 18:25:44 +0200

    modal input

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -76,14 +76,13 @@ document.getElementById("submitForm").addEventListener ("submit" , function (e) 
 	/*conditin IF ELSE for FIRST name input*/
 	// first name should not be EMPTY , LESS THEN 2 CHARACTERS or an SPACE character
 	
-	if (first.value == "" || first.value.lenght < 2) {
+	if (first.value == "" || first.value.length < 2) {
 		e.preventDefault();//stop form from submitting
 		firstError.style.display = "inline-block"
 		return false;
 
 	} else  if (/\s/.test(first.value)){ //stop form from submitting if there is space 
 		e.preventDefault();//stop form from submitting
-		console.log(first);
 		firstError.style.display = "inline-block"
 		return false;
 	}
@@ -92,14 +91,14 @@ document.getElementById("submitForm").addEventListener ("submit" , function (e) 
 	}
 	
 	/*conditin IF ELSE for LAST name input*/
-	if (last.value == "" || last.value.lenght < 2) {
+	if (last.value == "" || last.value.length < 2) {
 		e.preventDefault();//stop form from submitting
 		lastError.style.display = "inline-block"
 		return false;
+
 	} else  if (/\s/.test(last.value)){ //stop form from submitting if there is space 
 		e.preventDefault();//stop form from submitting
-		console.log(first);
-		firstError.style.display = "inline-block"
+		lastError.style.display = "inline-block"
 		return false;
 	}
 	 else {
